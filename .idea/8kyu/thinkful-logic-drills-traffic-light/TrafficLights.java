c0c1fd89e5ef4f36b706e4dab62e5fdcafdfa9bc
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2023-06-30 12:12:36 +0200

    created directories and added first kata

---
+++ .idea/8kyu/thinkful-logic-drills-traffic-light/TrafficLights.java
@@ -0,0 +1,15 @@
+public class TrafficLights {
+
+    public static String updateLight(String current) {
+        switch(current){
+            case "green":
+                return "yellow";
+            case "yellow":
+                return "red";
+            case "red":
+                break;
+        }
+        return "green";
+    }
+
+}
